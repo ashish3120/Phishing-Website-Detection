2a0c7ed633f28b24b0e535a8ebbcf9e9a665af11
ashish3120 <148547664+ashish3120@users.noreply.github.com>
2025-12-22 23:44:39 +0530

    added the deployment link

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://phishing-website-detection-ashy.vercel.app/"
         # add your frontend domain here after deployment
     ],
     allow_credentials=True,
